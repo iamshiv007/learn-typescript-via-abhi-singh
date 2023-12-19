861cf74b4dbecfadc802724185adf0be7fd38609
iamshiv007 <Github@#007>
2023-12-19 10:20:06 +0530

    ➕datatypes and functions

--- build/index.js
+++ build/index.js
@@ -1,4 +1,14 @@
 "use strict";
-let greet = 15;
-alert("Hello");
-greet = 10;
+// datatypes
+let num = 30;
+let myname = "vunay";
+let check = false;
+// Union datatypes
+let surname = true;
+surname = "jack";
+surname = 2;
+// typescript in function
+const func = (a, b) => {
+    return String(a + b);
+};
+func(3, 3);
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
-let greet:number = 15
+// datatypes
+let num = 30
+let myname: string = "vunay"
+let check: boolean = false
 
-alert("Hello")
+// Union datatypes
+let surname:any = true;
 
-greet = 10
+surname = "jack"
+surname = 2
+
+// typescript in function
+const func = (a:number, b:number):string => {
+    return String(a + b)
+}
+
+func(3, 3)
